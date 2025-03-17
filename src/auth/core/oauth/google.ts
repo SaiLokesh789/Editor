265bc41e33f9ed36201e6a932ff9fd1f98b552ad
Sai Lokesh <sailokesh@Sais-MacBook-Air-6.local>
2025-03-18 01:07:27 +0530

    rectified errors while adding google oauth

--- src/auth/core/oauth/google.ts
+++ src/auth/core/oauth/google.ts
@@ -2,27 +2,26 @@ import { env } from "@/data/env/server"
 import { OAuthClient } from "./base"
 import { z } from "zod"
 
-export function createGithubOAuthClient() {
+export function createGoogleOAuthClient() {
   return new OAuthClient({
     provider: "google",
     clientId: env.GOOGLE_CLIENT_ID,
     clientSecret: env.GOOGLE_CLIENT_SECRET,
     scopes: ["email", "profile"],
     urls: {
-      auth: "https://github.com/login/oauth/authorize",
-      token: "https://github.com/login/oauth/access_token",
-      user: "https://api.github.com/user",
+      auth: "https://accounts.google.com/o/oauth2/auth",
+      token: "https://oauth2.googleapis.com/token",
+      user: "https://www.googleapis.com/oauth2/v2/userinfo"
     },
     userInfo: {
       schema: z.object({
-        id: z.number(),
+        id: z.string(),
         name: z.string().nullable(),
-        login: z.string(),
         email: z.string().email(),
       }),
       parser: user => ({
         id: user.id.toString(),
-        name: user.name ?? user.login,
+        name: user.name ?? user.email,
         email: user.email,
       }),
     },
